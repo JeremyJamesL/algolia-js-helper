28656725117c784a0eaf253591b668a8f1506988
Jeremy Luscombe <jeremy.luscombe@LON-M1P-JLuscombe.local>
2022-10-15 15:59:33 +0100

    star rating and styles

--- frontend/index.js
+++ frontend/index.js
@@ -1,12 +1,25 @@
 var client = algoliasearch('YSWWVAX5RB', '9fb3db0222f7b5aef0e2b30791ee6201');
-var helper = algoliasearchHelper(client, 'restaurants', {facets: ['food_type', 'rounded_rating', 'stars_count'], maxValuesPerFacet: 5});
+var helper = algoliasearchHelper(client, 'restaurants', {facets: ['food_type', 'rounded_rating', 'payment_options'], maxValuesPerFacet: 5});
 
 // DOM
 const app = document.querySelector('.app');
 const searchInput = document.querySelector('.search-box');
 const resultsArea = document.querySelector('.results');
 const facetsArea = document.querySelector('.facets');
+const metaData = document.querySelector('.metadata');
 
+
+const successCallback = (position) => {
+    console.log(position);
+    console.log(position.coords.latitude, position.coords.longitude);
+    helper.setQueryParameter('aroundLatLng', `${position.coords.latitude},${position.coords.longitude}`);
+  };
+  
+  const errorCallback = (error) => {
+    console.log(error);
+  };
+  
+navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
 
 // Listen on search input
 searchInput.addEventListener('keydown', logQuery);
@@ -32,6 +45,8 @@ function updateFacets(e) {
 helper.search();
 
 helper.on('result', function(event) {
+    renderMetadata(event.results);
+    console.log(event);
     renderHits(event.results);
     renderFacetList(event.results);
 });
@@ -44,31 +59,74 @@ function renderHits(content) {
 
     const results = content.hits;
 
-    results.forEach(result => {
+    results.forEach(hit => {
         resultsArea.innerHTML += `
-            <li class="hit" id="${result.objectID}">
-                ${result.name}
+            <li class="hit" id=${hit.objectID}>
+                <div class="hit__head">
+                    <img src=${hit.image_url} class="hit__img"/>
+                </div>
+
+                <div class="hit__tail">
+                    <h2 class="hit__name">${hit.name}</h2>
+
+                    <div class="hit__ratrevs">
+                        <span class="hit__rating-count font--highlight">
+                            ${hit.stars_count}
+                        </span>
+                        <span class="hit__stars">
+                            ${renderStars(hit.stars_count, hit.objectID)}
+                        </span>
+                        <span class="hit__review-count font--light">
+                            (${hit.reviews_count} reviews)
+                        </span>
+                    </div>
+                    <div class="hit__info">
+                        <p class="font--light">
+                            ${hit.food_type} | ${hit.area} | ${hit.price_range}
+                        </p>
+                    </div>
+                </div>
             </li>
         `
     });
 }
 
 
+// Render stars 
+
+function renderStars(starsCount, id) {
+    console.log(starsCount,id)
+
+    const maxStars = 5;
+
+    const starPerc = (starsCount / maxStars) * 100;
+    const starPercRoun = `${(Math.round(starPerc / 10) * 10)}%`;
+    // document.querySelector(`#${id} .stars--inner`).style.width = starPercentageRounded;
+
+    return  (
+        `
+        <div class="stars--outer">
+            <div class="stars--inner" style="width: ${starPercRoun}">
+            </div>
+        </div>
+        `
+    )
+}
+
+
 // Render facets
 
 function renderFacetList(content) {
-
-
     const facetTypes = content.facets.map(facet => facet.name);
 
     let html = '';
 
     facetTypes.forEach(type => {
         const facetValues = content.getFacetValues(type);
-        html += `<h2 id=${type}>${type}</h2>
+        let typeTitle = type === 'food_type' ? 'Cuisine / Food Type' : type === 'rounded_rating' ? 'Rating' : type === "payment_options" ? 'Payment Options' : '';
+        html += `<h2 id=${type}>${typeTitle}</h2>
                  <ul id=${type}>
         `
-
         facetValues.forEach(value => {
             html += `
             <li>
@@ -82,6 +140,24 @@ function renderFacetList(content) {
         facetsArea.innerHTML = html;
     })
 
+}
+
+
+// Render metadata
+
+const renderMetadata = (content)  => {
+    const results = content.nbHits;
+    const timeSeconds = content.processingTimeMS / 1000;
+
+    let html = `
+        <span class="metadata__results">
+            ${results} results found
+        </span>
+        <span class="metadata__seconds"> 
+            in ${timeSeconds} seconds
+        </span>
+    `
+    metaData.innerHTML = html;
 }
 
 
